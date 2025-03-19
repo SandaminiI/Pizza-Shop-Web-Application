f6292985c5375c0296e8f50b69a86e2b2e463bbb
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2025-03-20 02:27:50 +0530

    Update invoice.go

--- backend/models/invoice.go
+++ backend/models/invoice.go
@@ -1,9 +1,35 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Invoice struct {
 	gorm.Model
-	CustomerID  uint    `json:"customer_id"`
-	TotalAmount float64 `json:"total_amount"`
+	CustomerID  uint           `json:"customer_id"`
+	Customer    Customer       `gorm:"foreignKey:CustomerID"`
+	Items       []InvoiceItem  `gorm:"foreignKey:InvoiceID"`
+	TotalAmount float64        `json:"total_amount"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
+}
+
+// type InvoiceItem struct {
+// 	gorm.Model
+// 	InvoiceItemID uint           `gorm:"primaryKey" json:"invoice_item_id"`
+// 	InvoiceID     uint           `json:"invoice_id"`
+// 	ItemID        uint           `json:"item_id"`
+// 	Item          Item           `gorm:"foreignKey:ItemID"`
+// 	Quantity      int            `json:"quantity"`
+// 	Subtotal      float64        `json:"subtotal"`
+// 	DeletedAt     gorm.DeletedAt `gorm:"index" json:"-"`
+// }
+
+type InvoiceItem struct {
+	InvoiceItemID uint           `gorm:"primaryKey;column:invoice_item_id" json:"invoice_item_id"` // Explicitly set as primary key
+	InvoiceID     uint           `json:"invoice_id"`
+	ItemID        uint           `json:"item_id"`
+	Item          Item           `gorm:"foreignKey:ItemID"`
+	Quantity      int            `json:"quantity"`
+	Subtotal      float64        `json:"subtotal"`
+	DeletedAt     gorm.DeletedAt `gorm:"index" json:"-"`
 }
