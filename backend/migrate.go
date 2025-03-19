b09a55f6d550dcdf3e5070f70cd0dcbc0784dd25
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2025-03-20 02:27:34 +0530

    Update migrate.go

--- backend/migrate.go
+++ backend/migrate.go
@@ -9,7 +9,7 @@ func RunMigrations() {
 	config.ConnectDatabase() // Connect to the database
 
 	// Automatically create tables based on models
-	config.DB.AutoMigrate(&models.Item{}, &models.Invoice{})
+	config.DB.AutoMigrate(&models.Item{}, &models.Invoice{}, &models.InvoiceItem{}, &models.Customer{})
 
 	// Add more models if necessary
 }
